90289b6375b625e59cd4a7d7c49830660d13b590
Abdul Ohab <abdulohab059@gmail.com>
2024-04-04 03:47:51 +0600

    changes

---
+++ Template/script.js
@@ -0,0 +1,6 @@
+// JavaScript code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+  // This function will be executed when the DOM is fully loaded
+  console.log("DOM is ready!");
+});
